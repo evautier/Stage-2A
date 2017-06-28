64a11a4d2b23834f291aecb64ea268459cc0724b
evautier <erwan.vautier@gmail.com>
2017-06-28 13:16:57 +0200

    Add files via upload

--- Gromov-Wasserstein/codes/gw/loss.py
+++ Gromov-Wasserstein/codes/gw/loss.py
@@ -70,6 +70,64 @@ def tensor_square_loss(C1,C2,T):
     return tens
     
     
+def tensor_kl_loss(C1,C2,T):
+    """
+    Returns the value of \mathcal{L}(C1,C2) \otimes T with the square loss 
     
+    function as the loss function of Gromow-Wasserstein discrepancy.
+    
+    Where :
+        
+        C1 : Metric cost matrix in the source space
+        C2 : Metric cost matrix in the target space
+        T : A coupling between those two spaces
+        
+    The square-loss function L(a,b)=(1/2)*|a-b|^2 is read as :
+        L(a,b) = f1(a)+f2(b)-h1(a)*h2(b) with :
+            f1(a)=a*log(a)-a
+            f2(b)=b
+            h1(a)=a
+            h2(b)=log(b)
+            
+    Parameters
+    ----------
+    C1 : np.ndarray(ns,ns)
+         Metric cost matrix in the source space
+    C2 : np.ndarray(nt,nt)
+         Metric costfr matrix in the target space
+    T :  np.ndarray(ns,nt)
+         Coupling between source and target spaces
+
+
+    Returns
+    -------
+    tens : (ns*nt) ndarray
+           \mathcal{L}(C1,C2) \otimes T tensor-matrix multiplication result
+           
+           
+    """
+    
+
+    C1=np.asarray(C1,dtype=np.float64)
+    C2=np.asarray(C2,dtype=np.float64)
+    T=np.asarray(T,dtype=np.float64)
+    
+    
+    def f1(a):
+        return a*np.log(a)-a
+    
+    def f2(b):
+        return b
+    
+    def h1(a):
+        return a
+    
+    def h2(b):
+        return np.log(b)
+    
+    tens=-np.dot(h1(C1),T).dot(h2(C2).T)
+    tens=tens-tens.min()
+    
+    return tens
     
     
--- Gromov-Wasserstein/codes/gw/updates.py
+++ Gromov-Wasserstein/codes/gw/updates.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def update_square_loss(p,lambdas,T,Cs):
     """
-    Updates C according to the L**2 Loss kernel with the S Ts couplings calculated at each iteration
+    Updates C according to the L2 Loss kernel with the S Ts couplings calculated at each iteration
     
     
     Parameters
@@ -31,4 +31,29 @@ def update_square_loss(p,lambdas,T,Cs):
     """
     somme=sum([lambdas[s]*np.dot(T[s].T,Cs[s]).dot(T[s]) for s in range(len(T))])
     ppt=np.dot(p,p.T)
-    return(np.divide(somme,ppt))  
+    return(np.divide(somme,ppt))
+
+def update_kl_loss(p,lambdas,T,Cs):
+    """
+    Updates C according to the KL Loss kernel with the S Ts couplings calculated at each iteration
+    
+    
+    Parameters
+    ----------
+    p  : np.ndarray(N,)
+         weights in the targeted barycenter
+    lambdas : list of the S spaces' weights
+    T : list of S np.ndarray(ns,N)
+        the S Ts couplings calculated at each iteration
+    Cs : Cs : list of S np.ndarray(ns,ns)
+         Metric cost matrices
+         
+    Returns 
+    ----------
+    C updated
+    
+    
+    """
+    somme=sum([lambdas[s]*np.dot(T[s].T,Cs[s]).dot(T[s]) for s in range(len(T))])
+    ppt=np.dot(p,p.T)
+    return(np.exp(np.divide(somme,ppt)))   
